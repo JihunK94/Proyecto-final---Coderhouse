878e2e4e877c0d90af25b81ccbe2951b92addaa4
Jihun <jihun.94@hotmail.com>
2024-05-29 18:14:28 -0300

    algunas cambios de funciones a clases

--- recetas/urls.py
+++ recetas/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from  recetas.views import index, lista_comidas, agregar_comidas, detalle_comidas
+from  recetas.views import index, detalle_comidas 
+from recetas.views import Ver_comidasList, Ver_comidasAgregar
 
 app_name = "recetas"
 
 urlpatterns =[
     path("", index, name="index"),
-    path("comidas/list", lista_comidas, name= "lista_comidas"),
-    path("comidas/create", agregar_comidas, name= "agregar_comidas"),
+    path("comidas/list", Ver_comidasList.as_view(), name= "lista_comidas"),
     path("comidas/detail/<int:pk>", detalle_comidas, name= "detalle_comidas"),
+    path("comidas/create", Ver_comidasAgregar.as_view(), name= "agregar_comidas"),
 ]
--- recetas/views.py
+++ recetas/views.py
@@ -1,36 +1,57 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from recetas.models import Ver_comidas
 from recetas.forms import ListacomidasForm
+from django.views.generic import CreateView, ListView, DeleteView, UpdateView, DetailView
+from django.urls import reverse_lazy
 
-def index(request): 
+def index(request):
     return render(request, "recetas/index.html")
 
-def lista_comidas(request):
-    busqueda = request.GET.get("busqueda", None)
-    if busqueda:
-        print(busqueda)
-        consulta = Ver_comidas.objects.filter(comidas__icontains=busqueda)
-    else:   
-        consulta = Ver_comidas.objects.all()  
-    contexto = {"comidas":consulta}
-    return render(request,"recetas/lista_comidas.html", contexto)
+# def lista_comidas(request):
+#     busqueda = request.GET.get("busqueda", None)
+#     if busqueda:
+#         print(busqueda)
+#         consulta = Ver_comidas.objects.filter(comidas__icontains=busqueda)
+#     else:
+#         consulta = Ver_comidas.objects.all()
+#     contexto = {"comidas":consulta}
+#     return render(request,"recetas/lista_comidas.html", contexto)
 
-def agregar_comidas(request):
-    if request.method == "POST":
-        form =  ListacomidasForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect ("recetas:lista_comidas")
-    else:
-        form = ListacomidasForm()
-        return render (request, "recetas/lista_comidas_agregar.html", {"form": form} )
-    
+class Ver_comidasList(ListView):
+    model = Ver_comidas
+    #context_object_name: "comidas"
+    template_name = "recetas/lista_comidas.html"
+    def get_queryset(self) -> QuerySet[Any]:
+        if self.request.GET.get("busqueda"):
+            busqueda = self.request.GET.get("busqueda")
+            consulta = Ver_comidas.objects.filter(comidas__icontains=busqueda)
+        else:
+            consulta = Ver_comidas.objects.all()
+        return consulta
+
+# def agregar_comidas(request):
+#      if request.method == "POST":
+#          form =  ListacomidasForm(request.POST)
+#          if form.is_valid():
+#              form.save()
+#              return redirect ("recetas:lista_comidas")
+#      else:
+#          form = ListacomidasForm()
+#          return render (request, "recetas/lista_comidas_form.html", {"form": form} )
+
+class Ver_comidasAgregar(CreateView):
+     model = Ver_comidas
+     form_class = ListacomidasForm
+     template_name = "recetas/lista_comidas_form.html"
+     success_url = reverse_lazy("recetas:lista_comidas")
+
+
 def detalle_comidas(request, pk: int):
     consulta = Ver_comidas.objects.get(id=pk)
     contexto = {"detalle" : consulta}
     return render (request, "recetas/lista_comidas_detalles.html", contexto)
-    
-
 
 
 
